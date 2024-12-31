8da5cde18d4ab01970e2173623b51c621d6f39a5
weizmanfabian <weizmanfabian@gmail.com>
2024-12-31 14:04:59 -0500

    Enhance useForm hook with detailed JSDoc comments for better documentation and understanding of form handling functions

--- universidad-front/src/hooks/useForm.js
+++ universidad-front/src/hooks/useForm.js
@@ -1,27 +1,48 @@
-import { useState } from "react";
-
+/**
+ * Hook para manejar formularios
+ * @param {Object} initialForm Formulario inicial
+ * @param {Function} validateForm Función para validar el formulario
+ * @returns {Object} Objeto con las siguientes propiedades:
+ *  - form: estado del formulario
+ *  - setForm: función para actualizar el formulario
+ *  - errors: errores del formulario
+ *  - setErrors: función para actualizar los errores
+ *  - handleChange: función para manejar cambios en el formulario
+ *  - handleBlur: función para manejar el evento blur en el formulario
+ *  - handleSubmit: función para manejar el envío del formulario
+ *  - isSubmitting: estado de envío del formulario
+ *  - setIsSubmitting: función para actualizar el estado de envío
+ *  - validateErrors: función para validar errores en un campo
+ */
 export const useForm = (initialForm, validateForm) => {
   const [form, setForm] = useState(initialForm);
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
 
+  /**
+   * Función para validar errores en un campo
+   * @param {string} value Valor del campo
+   * @param {Array} expresionesRegulares Expresiones regulares para validar
+   * @param {Array} messages Mensajes de error
+   * @returns {string} Error del campo
+   */
   const validateErrors = (value, expresionesRegulares, messages) => {
-    let res = '';
-
-    expresionesRegulares.forEach((ex, index) => {
+    const results = expresionesRegulares.map((ex, index) => {
       if (!ex.test(value)) {
-        res = messages[index];
+        return messages[index];
       }
+
+      return "";
     });
 
-    if (!value) {
-      res = "Campo requerido";
-    }
-
-    return res;
+    return results.find((result) => result !== "");
   };
 
-
+  /**
+   * Función para manejar cambios en el formulario
+   * @param {Event} e Evento de cambio
+   * @param {Function} callback Función a ejecutar después de cambiar el formulario
+   */
   const handleChange = async (e, callback) => {
     const { name, value } = e.target;
 
@@ -33,6 +54,11 @@ export const useForm = (initialForm, validateForm) => {
     if (callback) await callback();
   };
 
+  /**
+   * Función para manejar el evento blur en el formulario
+   * @param {Event} e Evento de blur
+   * @param {Function} callback Función a ejecutar después de cambiar el formulario
+   */
   const handleBlur = async (e, callback) => {
     handleChange(e);
     setErrors(validateForm(form));
@@ -40,9 +66,16 @@ export const useForm = (initialForm, validateForm) => {
     if (callback) await callback();
   };
 
+  /**
+   * Función para manejar el envío del formulario
+   * @param {Event} e Evento de envío
+   * @param {Function} sendPostPutForm Función a ejecutar para enviar el formulario
+   * @param {Function} actionPrev Función a ejecutar antes de enviar el formulario
+   * @param {Function} actionPost Función a ejecutar después de enviar el formulario
+   */
   const handleSubmit = async (
     e,
-    sendPostPutForm, // Función a ejecutar (POST, PUT o cualquier otra)
+    sendPostPutForm,
     actionPrev = null,
     actionPost = null
   ) => {
@@ -51,20 +84,10 @@ export const useForm = (initialForm, validateForm) => {
     if ((errResult && Object.values(errResult).some((error) => error !== "")) ||
       JSON.stringify(form) !== JSON.stringify(initialForm)) {
       try {
-        setIsSubmitting(true); // Inicia el estado de envío
-
-        // Swal.fire({
-        //   title: "...!",
-        //   html: "Espere mientras se cargan los datos...",
-        //   timerProgressBar: false,
-        //   didOpen: () => {
-        //     Swal.showLoading();
-        //   },
-        // });
+        setIsSubmitting(true);
 
         if (actionPrev) await actionPrev();
 
-        // Ejecutar la función pasada como sendPostPutForm
         await sendPostPutForm();
 
         if (actionPost) await actionPost();
@@ -72,15 +95,13 @@ export const useForm = (initialForm, validateForm) => {
       } catch (err) {
         console.error(`Ocurrió un error: ${err}`);
       } finally {
-        //Swal.close();
-        setIsSubmitting(false); // Finaliza el estado de envío
+        setIsSubmitting(false);
       }
     } else {
       setErrors(errResult);
       return false;
     }
   };
-
 
   return {
     form,
@@ -92,6 +113,7 @@ export const useForm = (initialForm, validateForm) => {
     handleSubmit,
     isSubmitting,
     setIsSubmitting,
-    validateErrors
+    validateErrors,
   };
 };
+
